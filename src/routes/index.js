d7d3c9dbc5ef018d7172e4a26a1e9c777c7f819c
jerrytsengz <jerrytseng0@gmail.com>
2023-10-31 23:12:49 -0700

    About page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 // import HomePage from "../page/HomePage";
 // import ProductPage from "../page/ProductPage";
+// import AboutPage from "../page/AboutPage";
 
 // const publicRoutes = [
 //   {path: "/", component: HomePage},
-//   {path: "/product",component: ProductPage}
+//   {path: "/product",component: ProductPage},
+//   {path: "/about", component: AboutPage}
 // ]
 
 // export {publicRoutes}
