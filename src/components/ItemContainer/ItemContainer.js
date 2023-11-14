ac7d91e4e67a86fcd5371f16f8040573086f349e
vinhnguyen1402 <andyngo1402@gmail.com>
2023-11-13 21:51:24 -0800

    update

--- src/components/ItemContainer/ItemContainer.js
+++ src/components/ItemContainer/ItemContainer.js
@@ -1,21 +1,32 @@
 import { memo } from "react";
 import "./ItemContainer.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory, useParams } from "react-router-dom";
 
 const ItemContainer = memo((props) => {
+  // const history = 
+
   const handleAddToCart = () => {
     // props.addToCart(props)
     console.log("Add to Cart");
   };
+  // const handleOnClick = () => {
+  //   history.push(`/product/${props.id}/description`);
+  // };
   return (
-    <div className="item">
+    <div className="item" >
       <Link className="linkTag" to={`/product/${props.id}/description`}>
-        <img className="productImg" src={props.image} alt="product" />
-        <p className="productName">{props.productName}</p>
-        <p className="review"> {props.review} reviews</p>
-        <p className="price">$ {props.price}</p>
+      <img className="productImg" src={props.image} alt="product" />
+      <p className="productName">{props.productName}</p>
+      <p className="review"> {props.review} reviews</p>
+      <p className="price">$ {props.price}</p>
       </Link>
-      <button className="addToCartButton" onClick={handleAddToCart}>
+      <button
+        className="addToCartButton"
+        onClick={(e) => {
+          e.stopPropagation();
+          handleAddToCart();
+        }}
+      >
         Add to Cart
       </button>
     </div>
