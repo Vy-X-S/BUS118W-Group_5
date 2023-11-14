f6bb671cadc54cc76896eaaee27bf4c447441653
vinhnguyen1402 <andyngo1402@gmail.com>
2023-11-13 22:57:01 -0800

    update

---
+++ src/components/AddToCartButton/AddToCart.js
@@ -0,0 +1,27 @@
+import { memo } from "react";
+import { Link, useHistory, useParams } from "react-router-dom";
+import "./AddToCart.css";
+const AddToCartButton = memo((props) => {
+  // const history =
+
+  const handleAddToCart = () => {
+    // props.addToCart(props)
+    console.log("Add to Cart");
+  };
+  // const handleOnClick = () => {
+  //   history.push(`/product/${props.id}/description`);
+  // };
+  return (
+    <button
+      className="addToCartButton"
+      onClick={(e) => {
+        e.stopPropagation();
+        handleAddToCart();
+      }}
+    >
+      Add to Cart
+    </button>
+  );
+});
+
+export default AddToCartButton;
--- src/components/ItemContainer/ItemContainer.js
+++ src/components/ItemContainer/ItemContainer.js
@@ -1,34 +1,27 @@
 import { memo } from "react";
 import "./ItemContainer.css";
-import { Link, useHistory, useParams } from "react-router-dom";
+import { Link, useHistory, useParams, useNavigate } from "react-router-dom";
+import AddToCartButton from "../AddToCartButton/AddToCart";
 
 const ItemContainer = memo((props) => {
-  // const history = 
+  const navigate = useNavigate();
 
   const handleAddToCart = () => {
     // props.addToCart(props)
     console.log("Add to Cart");
   };
-  // const handleOnClick = () => {
-  //   history.push(`/product/${props.id}/description`);
-  // };
+  const handleOnClick = () => {
+    navigate(`/product/${props.id}/description`);
+  };
   return (
-    <div className="item" >
-      <Link className="linkTag" to={`/product/${props.id}/description`}>
+    <div className="item" onClick={handleOnClick}>
+      {/* <Link className="linkTag" to={`/product/${props.id}/description`}> */}
       <img className="productImg" src={props.image} alt="product" />
       <p className="productName">{props.productName}</p>
       <p className="review"> {props.review} reviews</p>
       <p className="price">$ {props.price}</p>
-      </Link>
-      <button
-        className="addToCartButton"
-        onClick={(e) => {
-          e.stopPropagation();
-          handleAddToCart();
-        }}
-      >
-        Add to Cart
-      </button>
+      {/* </Link> */}
+      <AddToCartButton></AddToCartButton>
     </div>
   );
 });
